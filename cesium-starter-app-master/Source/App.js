99f9c37241bcc421711805e09d011983a8f54e6a
maramaki <m3aramaki@gmail.com>
2015-11-22 11:43:25 +0900

    test_01

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,9 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
+var point = viewer.entities;
+point.add({
+position : Cesium.Cartesian3.fromDegrees(135.502101, 34.693684),
+point : {
+pixelSize : 10,
+color : Cesium.Color.YELLOW
+}
+});
